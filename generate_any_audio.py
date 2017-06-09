25023bb4f6320a5bd7d8f1774351239982c75b8c
153armstrong <sriram960326@gmail.com>
2017-06-09 20:20:28 +0530

    Create generate_any_audio.py

---
+++ generate_any_audio.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy import signal as sg
+
+Fs = 44100                    ## Sampling Rate
+f = 440                       ## Frequency (in Hz)
+sample = 44100                ## Number of samples 
+x = np.arange(sample)
+
+####### sine wave ###########
+y = 100*np.sin(2 * np.pi * f * x / Fs)
+
+####### square wave ##########
+# y = 100* sg.square(2 *np.pi * f *x / Fs )
+
+####### square wave with Duty Cycle ##########
+# y = 100* sg.square(2 *np.pi * f *x / Fs , duty = 0.8)
+
+####### Sawtooth wave ########
+# y = 100* sg.sawtooth(2 *np.pi * f *x / Fs )
+
+
+f = open('test.wav','wb')
+## Open as Signed 8-bit on Audacity - Watch Video for instructions
+
+for i in y:
+	print i
+	f.write(struct.pack('b',i))
+f.close()
+
