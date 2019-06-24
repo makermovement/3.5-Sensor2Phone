0d409676d8fbc4aa2165421416566c2beba274c3
Mirko Chialastri <chialastri.mirko@gmail.com>
2019-06-24 22:00:57 +0200

    wave headers

--- generate_any_audio.py
+++ generate_any_audio.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
 import struct
 import numpy as np
 from scipy import signal as sg
+import wave
 
 sampling_rate = 44100                    ## Sampling Rate
-freq = 440                               ## Frequency (in Hz)
-samples = 44100                          ## Number of samples 
+freq          = 440                      ## Frequency (in Hz)
+samples       = 44100                    ## Number of samples 
 x = np.arange(samples)
 
 ####### sine wave ###########
@@ -20,12 +22,14 @@ y = 100*np.sin(2 * np.pi * freq * x / sampling_rate)
 # y = 100* sg.sawtooth(2 *np.pi * f *x / Fs )
 
 
-f = open('test.wav','wb')
+f = wave.open('test.wav','wb')
+f.setparams((1, 2, 22050, 59071, 'NONE', 'not compressed'))
+
 ## Instructions to play test.wav on computer
 ## 1. Open as Signed 8-bit on Audacity - Watch Video at https://bit.ly/2YwmN9q for instructions
 ## 2. Or using SoX: play -t raw -r 44.1k -e signed -b 8 -c 1 test.wav
 
 for i in y:
-	f.write(struct.pack('b',int(i)))
-f.close()
+	f.writeframes(struct.pack('b',int(i)))
 
+f.close()
